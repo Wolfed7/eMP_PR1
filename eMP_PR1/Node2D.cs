6616d1f8b693e7982c35ace5363729e5e8216ca1
Дмитрий Щукин <shhukin.2020@azure.nstu.ru>
2023-02-20 15:23:55 +0700

    Добавлен класс Node2D

---
+++ eMP_PR1/Node2D.cs
@@ -0,0 +1,29 @@
+﻿namespace eMP_PR1;
+
+public enum BoundaryType
+{
+   Dirichlet,  // Краевое первого типа.
+   Neumann     // Второго.
+}
+
+public enum NodeType
+{
+   Boundary,
+   Inner,
+   Fake
+}
+
+public class Node2D
+{
+   // Положение в декартовых координатах.
+   public double X { get; init; }
+   public double Y { get; init; }
+
+   // Положение в матрице МКР.
+   public int I { get; init; }
+   public int J { get; init; }
+
+   public NodeType NodeType { get; set; } 
+
+   public BoundaryType BoundaryType { get; set; }
+}
